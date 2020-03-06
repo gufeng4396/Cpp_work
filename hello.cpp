75306ae6001ee9947c3f37b3747baa817bfd2097
gu_feng <48351291+gufeng4396@users.noreply.github.com>
2020-03-06 10:54:13 +0800

    ok

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(void)
+{
+    int a = 3;
+    int b[3] = {3, 5, 2};
+    std::cout << "hello";
+    sort(b,b+3);
+    a = 5;
+    return 0;
+}
